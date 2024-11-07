47199b60c46314de5a980422c6a33674cbc8dd5f
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-11-07 18:26:21 +0200

    update user service

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,10 +1,22 @@
 import { PrismaClient, Prisma, User } from '@prisma/client';
+import   bcrypt   from 'bcrypt';
 
 const prisma = new PrismaClient();
 
 export class UserService {
-  async createUser(userData: Prisma.UserCreateInput): Promise<void> {
-    await prisma.user.create({ data: userData });
+  async createUser(name: string, email: string, password: string, role: 'ADMIN' | 'USER' | 'TENANT'): Promise<User> {
+ 
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        role
+      }
+    })
+    return newUser;
   }
 
   async getAllUsers(): Promise<User[]> {
