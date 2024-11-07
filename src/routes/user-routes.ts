9226f0782df928bc03a29eb328fbe66af21613b3
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-11-07 18:26:06 +0200

    update user routes

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,11 +1,14 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import { UserController } from '../controllers/user-controller';
 const userController = new UserController();
 
 const router = express.Router();
 
-router.post('/', async (req: Request, res: Response) => {
-  await userController.createUser(req, res);
+router.post('/', async (req: Request, res: Response, next: NextFunction) => {
+  await userController.createUser(req, res, next);
+});
+router.get('/', async (req: Request, res: Response, next: NextFunction) => {
+  await userController.getAllUsers(req, res, next);
 });
 
 export default router;
