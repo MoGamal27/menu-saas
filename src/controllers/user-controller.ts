832281a89e6b8f8d75dd1850158b711d43f48e32
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-11-07 18:25:48 +0200

    update user controller

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,11 +1,22 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { UserService } from "../services/user-service";
+import asyncHandler from "express-async-handler"
 
 export class UserController {
-  async createUser(req: Request, res: Response) {
-    const userData = req.body;
-    const userService = new UserService();
-    await userService.createUser(userData);
-    res.status(201).json({ message: "User created successfully" });
+
+  private userService: UserService;
+
+  constructor() {
+    this.userService = new UserService();
   }
+  createUser = asyncHandler(async(req: Request, res: Response, next: NextFunction) => {
+    const { name, email, password, role } = req.body;
+   const user = await  this.userService.createUser(name, email, password, role);
+    res.status(201).json({ message: "User created successfully", user });
+  });
+
+  getAllUsers = asyncHandler(async(req: Request, res: Response, next: NextFunction) => {
+    const users = await this.userService.getAllUsers();
+    res.status(200).json({ message: "Users retrieved successfully", users });
+  })
 }
