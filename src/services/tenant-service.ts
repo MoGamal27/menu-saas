9060c5ba8fbe49a466ef977dba5abe3a6b785634
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-11-10 19:04:35 +0200

    update service teanat

--- src/services/tenant-service.ts
+++ src/services/tenant-service.ts
@@ -1,37 +1,15 @@
-import { PrismaClient, Prisma, Tenant } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+
+const prisma = new PrismaClient()
 
 export class TenantService{
-
-    async createTenant(name: string, subdomain: string) {
-        
-        const newTenant = await prisma.tenant.create({
-            data: {
-                name,
-                subdomain
-            }
-        })
-        return newTenant;
-    }
-
-    async getAllTenants() {
-        return await prisma.tenant.findMany();
-    }
-
-    async getTenantBySubdomain(subdomain: string) {
-        return await prisma.tenant.findUnique({
-            where: {
-                subdomain
-            }
-        })
-    }
-    async getTenantById(id: number) {
-        return await prisma.tenant.findUnique({
-            where: {
-                id
-            }
-        })
-    }
+   async createTenant(name: string, subdomain: string) {
+    const tenant = await prisma.tenant.create({
+        data: { name, subdomain }
+    });
+     
+    return tenant;
+}
     
 }
