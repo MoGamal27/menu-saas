94cac1f7a32e5385d852d9c473ee2527d5dfee7b
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-11-10 19:05:10 +0200

    Update tenant-routes.ts

--- src/routes/tenant-routes.ts
+++ src/routes/tenant-routes.ts
@@ -4,12 +4,9 @@ import { TenantController } from '../controllers/tenant-controller';
 const router = express.Router();
 const tenantController = new TenantController();
 
-router.get('/', tenantController.getAllTenants);
-   
-router.get('/:subdomain', tenantController.getTenantBySubdomain);
 
 router.post('/', tenantController.createTenant);
 
-router.get('/:id', tenantController.getTenantById);
+
 
 export default router;
