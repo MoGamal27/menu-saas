bc62b2aebc27e87773128d26b27a524f37c6f024
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-11-10 19:03:39 +0200

    update middleware

--- src/middleware/tenant-middleware.ts
+++ src/middleware/tenant-middleware.ts
@@ -1,40 +1,19 @@
-import { Request, Response, NextFunction } from "express";
-import { PrismaClient } from "@prisma/client";
-import { getTenantPrisma } from "../../prisma/tenantPrisma";
+import { Request, Response, NextFunction} from 'express';
+import { PrismaClient } from '@prisma/client';
 
-interface RequestWithTenantPrisma extends Request {
-    tenantPrisma: ReturnType<typeof getTenantPrisma>;
-}
-
-const prisma = new PrismaClient();
-
-export const tenantMiddleware = async (
-    req: RequestWithTenantPrisma,
-    res: Response,
-    next: NextFunction
-) => {
-    const subdomain = req.headers['x-tenant'] as string;
-
-    if (!subdomain) {
-        return res.status(400).json({
-            message: "No tenant provided"
-        });
+/*declare global {
+  namespace Express {
+    interface Request {
+      tenantId: string;
+      prisma: PrismaClient;
     }
+  }
+}*/
 
-    const tenant = await prisma.tenant.findUnique({
-        where: {
-            subdomain
-        }
-    });
-    if (!tenant) {
-        return res.status(400).json({
-            message: "Tenant not found"
-        });
-    }
 
-        if (tenant) {
-        req.tenantPrisma = getTenantPrisma(`tenant_${tenant.id}`);
-        return req.tenantPrisma;
-    }
-    next();
+
+export const tenantMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  req.headers['x-tenant-id'] = req.headers['x-tenant-id'] || 'public';
+  next();
 };
+
