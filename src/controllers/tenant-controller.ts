ce3a7f4815156543ba94d8bc9b488819fd282be9
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-11-10 19:03:22 +0200

    update tenant

--- src/controllers/tenant-controller.ts
+++ src/controllers/tenant-controller.ts
@@ -1,34 +1,16 @@
 import { Request, Response, NextFunction } from "express";
 import asyncHandler from "express-async-handler";
 import { TenantService } from "../services/tenant-service";
+ 
+const tenantService = new TenantService();
 
 export class TenantController {
-    private tenantService: TenantService;
-    constructor() {
-        this.tenantService = new TenantService();
-    }
 
   createTenant = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     const { name, subdomain} = req.body;
-    const newTenant = await this.tenantService.createTenant(name, subdomain);
+    const newTenant = await tenantService.createTenant(name, subdomain);
         res.status(201).json({ message: "Tenant created successfully", tenant: newTenant });
   })
 
-  getAllTenants = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    const tenants = await this.tenantService.getAllTenants();
-    res.status(200).json({ message: "Tenants fetched successfully", tenants });
-  })
-
-  getTenantBySubdomain = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    const { subdomain } = req.params;
-    const tenant = await this.tenantService.getTenantBySubdomain(subdomain);
-    res.status(200).json({ message: "Tenant fetched successfully", tenant });
-  })
-
-  getTenantById = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    const { id } = req.params as any;
-    const tenant = await this.tenantService.getTenantById(id);
-    res.status(200).json({ message: "Tenant fetched successfully", tenant });
-  })
-
+  
 }
